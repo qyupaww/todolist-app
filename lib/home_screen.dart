449291cc02f23b1616a5035a97d1a76a416b3a62
Faisal Rahman <qyupaww@gmail.com>
2025-11-13 16:30:23 +0700

    feat: UI Screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:todolist_app/widgets/tile_widget.dart';
+import 'constants.dart';
+
+class TodoHomeScreen extends StatefulWidget {
+  const TodoHomeScreen({super.key});
+
+  @override
+  State<TodoHomeScreen> createState() => _TodoHomeScreenState();
+}
+
+class _TodoHomeScreenState extends State<TodoHomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorPallete.black,
+      appBar: AppBar(
+        backgroundColor: ColorPallete.black,
+        scrolledUnderElevation: 0,
+        elevation: 0,
+        actions: const [
+          Icon(Icons.more_horiz_outlined, color: ColorPallete.red),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Today",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: ColorPallete.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "13 Nov - Thu",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: ColorPallete.white,
+              ),
+            ),
+          ),
+          const Divider(thickness: 0.1, color: ColorPallete.grey),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    TodoTileWidget(todo: DummyData.taskList[index]),
+                    const SizedBox(height: 16),
+                    const Divider(
+                      thickness: 0.1,
+                      color: ColorPallete.grey,
+                      indent: 40,
+                    ),
+                  ],
+                );
+              },
+              itemCount: DummyData.taskList.length,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: ColorPallete.red,
+        onPressed: () {},
+        tooltip: 'Add Task',
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add, color: ColorPallete.white),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: ColorPallete.blackLight,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, color: ColorPallete.white),
+            label: "Today",
+            activeIcon: Icon(Icons.calendar_today, color: ColorPallete.red),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox, color: ColorPallete.white),
+            label: "Inbox",
+            activeIcon: Icon(Icons.inbox, color: ColorPallete.red),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: ColorPallete.white),
+            label: "Search",
+            activeIcon: Icon(Icons.search, color: ColorPallete.red),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article, color: ColorPallete.white),
+            label: "Browse",
+            activeIcon: Icon(Icons.article, color: ColorPallete.red),
+          ),
+        ],
+        selectedItemColor: ColorPallete.red,
+        unselectedItemColor: ColorPallete.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
+void main() {
+  runApp(const TodolistApp());
+}
+
+class TodolistApp extends StatelessWidget {
+  const TodolistApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Todo List App', home: const TodoHomeScreen());
+  }
+}
